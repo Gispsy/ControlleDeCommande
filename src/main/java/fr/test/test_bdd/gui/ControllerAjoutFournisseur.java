26726e059776cf34000171c39db2fe13941febee
Gispsy <mordredbta3011@gmail.com>
2023-01-24 14:09:22 +0100

    Verification regex presque fini avec bonne implementation de l'id

--- src/main/java/fr/test/test_bdd/gui/ControllerAjoutFournisseur.java
+++ src/main/java/fr/test/test_bdd/gui/ControllerAjoutFournisseur.java
@@ -11,6 +11,8 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 import java.sql.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ControllerAjoutFournisseur {
 
@@ -25,24 +27,64 @@ public class ControllerAjoutFournisseur {
     public Button BtnAjout;
     public Button BtnAnnuler;
     private static AtomicInteger count = new AtomicInteger();
+    private int LastID;
 
     //Ajout a la base de donnée les valeur entrer avec verification regex
     public void BtnAjoutClick(ActionEvent actionEvent){
+        try{
+            String url = "jdbc:mysql://localhost:3307/papyrus";
+            Connection con = DriverManager.getConnection(url, "root", "tiger");
+            String sql ="SELECT numfou FROM fournis ORDER BY numfou DESC LIMIT 1";
+            Statement stm = con.createStatement();
+            ResultSet res = stm.executeQuery(sql);
+
+            while (res.next()){
+                LastID = res.getInt("numfou") + 1;
+
+            }
+
+            stm.execute(sql);
+            stm.close();
+            con.close();
+
+
+        }catch (Exception e){                                           //Si la connection a la base de donnée ne fonctionne pas
+            e.getMessage();                                             //Message d'erreur
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);       //Fenetre d'aller d'information
+            alert.setTitle("Mauvaise connection !");                    //Titre Fenetre d'alerte
+            alert.setContentText(e.getMessage());                     //Savoir precisement l'alerte
+            alert.showAndWait();                                        //Fais apparaitre la fenetre et attend une validation de l'utilisateur
+
+        }
 
         try{
         //Connection BDD
         String url = "jdbc:mysql://localhost:3307/papyrus";
         Connection con = DriverManager.getConnection(url, "root", "tiger");
 
-        int count = 9180;
-        count++;
-
         //Creation objet pour effectuer des requêtes
 
         PreparedStatement stm = con.prepareStatement("INSERT INTO fournis (numfou, nomfou, ruefou, vilfou, posfou, confou) " +
                                                         "VALUES (?, ?, ?, ?, ?, ?)");
+        Pattern p, p2, p3, p4, p5;
+        Matcher m, m2, m3, m4, m5;
 
-        stm.setInt(1, count);
+        //verification regex
+        p = Pattern.compile("^[A-Z]\\D*\\S[^a-z0123456789] *$");
+        m = p.matcher(TextFieldNom.getText());
+        p2 = Pattern.compile("^[0-9]{2}[a-z]*\\D*\\S[^0123456789] *$");
+        m2 = p2.matcher(TextFieldRue.getText());
+        p3 = Pattern.compile("^[A-Z]{1}[a-z]*\\D*\\S[^0123456789] *$");
+        m3 = p3.matcher(TextFieldVille.getText());
+        p4 = Pattern.compile("");
+        m4 = p4.matcher(TextFieldCP.getText());
+        p5 = Pattern.compile("^[A-Z]{1}[a-z]*\\D*\\S[^0123456789] *$");
+        m5 = p5.matcher(TextFieldContact.getText());
+
+        if(m.find() && m2.find()){
+
+
+        stm.setInt(1, LastID);
         stm.setString(2, TextFieldNom.getText());
         stm.setString(3, TextFieldRue.getText());
         stm.setString(4, TextFieldVille.getText());
@@ -57,6 +99,12 @@ public class ControllerAjoutFournisseur {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation");
         alert.showAndWait();
+        }else{
+
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Mauvaise entrer dans le formulaire");
+            alert.showAndWait();
+        }
 
 
     }catch (Exception e){                                           //Si la connection a la base de donnée ne fonctionne pas
